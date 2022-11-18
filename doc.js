03eae69a046120f4a571190356b3f1b8ce052ea1
davido-2003 <118538031+davido-2003@users.noreply.github.com>
2022-11-18 13:52:22 +0100

    Add files via upload

---
+++ doc.js
@@ -0,0 +1,62 @@
+function m() {
+    calcu.display.value=""
+}
+function my() {
+    calcu.display.value=""
+}
+
+function myf() {
+    calcu.display.value+="%"
+}
+function myfu() {
+    calcu.display.value+="+"
+}
+function myfun() {
+    calcu.display.value+="7"
+}
+function myfunc() {
+    calcu.display.value+="8"
+}
+function myfunct() {
+    caalcu.display.value+="9"
+}
+function myfuncti() {
+    calcu.display.value+="*"
+}
+function myfunctio() {
+    calcu.display.value+="4"
+}
+function myfunction() {
+    calcu.display.value+="5"
+}
+function myF() {
+    calcu.display.value+="6"
+}
+function myFu() {
+    calcu.display.value+="-"
+}
+function myFun() {
+    calcu.display.value+="1"
+}
+function myFunc() {
+    calcu.display.value+="2"
+}
+function myFunct() {
+    calcu.display.value+="3"
+}
+function myFuncti() {
+    calcu.display.value+="+"
+}
+function myFunctio() {
+    calcu.display.value+="0"
+}
+function myFunction() {
+    calcu.display.value+="."
+}
+function myFunctions() {
+    calcu.display.value = eval(calcu.display.value)
+}
+
+var dave = "I am a boldlinks student"
+slc = dave.toUpperCase("b")
+console.log(slc);
